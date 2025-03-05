d34e2fa02148d88213b190a910014d93345a533d
amireskandari <97438215+amireskandari3035@users.noreply.github.com>
2025-03-05 14:40:11 +0330

    prod databse change

--- database.py
+++ database.py
@@ -2,11 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://project_database_8mld_user:rIWT91aM9Ou3buihkN9ClGkzyRcdoF2E@dpg-cv42v7jtq21c73an7j10-a.frankfurt-postgres.render.com/project_database_8mld"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
+
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+#
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
